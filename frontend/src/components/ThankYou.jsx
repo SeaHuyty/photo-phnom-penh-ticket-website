fae8d2afbf1daab7c86b5592b860d8c280e86c7a
Visnuka <sophavisnukakhun@gmail.com>
2025-03-22 20:39:51 +0700

    All done except Admin Dashboard

--- frontend/src/components/ThankYou.jsx
+++ frontend/src/components/ThankYou.jsx
@@ -3,20 +3,12 @@ import React from "react";
 function ThankYou() {
   return (
     <div className="thankYou" >
-      <h1>Thank You for Your Purchase!</h1>
-      <p>Your ticket will be sent to your email shortly.</p>
+      <div className="text">
+        <h1>Thank You for Your Purchase!</h1>
+        <p>Your ticket will be sent to your email shortly.</p>
+      </div>
     </div>
   );
 }
-
-// const styles = {
-//   container: {
-//     textAlign: "center",
-//     padding: "50px",
-//     display: "flex",
-//     flexDirection: "column",
-//     alignItems: "center",
-//   },
-// };
 
 export default ThankYou;
